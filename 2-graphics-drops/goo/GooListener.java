2d151004461aa9a8a93695c272ec47852bbbc369
Oskar Enmalm <oskarenmalm@gmail.com>
2018-11-01 00:38:54 +0100

    cleaning up packages structure

--- 2-graphics-drops/goo/GooListener.java
+++
@@ -1,9 +0,0 @@
-/**
- * All listeners must implement this interface.
- * @author Tim Blackwell
- *
- */
-public interface GooListener {
-
-    public abstract void gooActionPerformed(GooEvent e);
-}
